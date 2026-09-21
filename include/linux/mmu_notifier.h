6290c18f7fac57f8a58f841e30292c35a95e6bfe
Kernel Builder <builder@localhost>
2026-09-21 11:44:45 +0000

    chore: remove backup files and build artifacts

--- include/linux/mmu_notifier.h
+++ include/linux/mmu_notifier.h
@@ -342,6 +342,19 @@ static inline void mmu_notifier_mm_destroy(struct mm_struct *mm)
 }
 
 
+/* mmu_notifier_event - reason for the mmu notifier callback */
+enum mmu_notifier_event {
+	MMU_NOTIFY_UNMAP = 0,
+	MMU_NOTIFY_CLEAR,
+	MMU_NOTIFY_PROTECTION_VMA,
+	MMU_NOTIFY_PROTECTION_PAGE,
+	MMU_NOTIFY_SOFT_DIRTY,
+	MMU_NOTIFY_RELEASE,
+	MMU_NOTIFY_MIGRATE,
+	MMU_NOTIFY_EXCLUSIVE,
+};
+
+
 static inline void mmu_notifier_range_init(struct mmu_notifier_range *range,
 					   enum mmu_notifier_event event,
 					   unsigned flags,
