bfbf3bb042ba44d68b95cd4c521daf905d8993ec
Kernel Builder <builder@localhost>
2026-09-21 14:32:39 +0000

    kernel: fix additional build errors from GKI backports
    1. Fix btrfs/zstd.c to use kernel zstd API wrapper names instead of
       raw ZSTD names that don't exist in available zstd version
    
    2. Fix kernel/sysctl.c: wrap bpf_stats_handler table entry in
       CONFIG_BPF_SYSCALL guard (BPF_SYSCALL is disabled on this kernel)
    
    3. Fix fanotify.c: add missing old_memcg variable declaration
    
    4. Fix task_mmu.c: correct split_huge_pmd argument order (vma, pmd, addr)
    
    5. Guard RCU trace function usage in tasks.h for CONFIG_TASKS_TRACE_RCU
    
    6. Enable CONFIG_TASKS_TRACE_RCU for proper RCU trace support
    
    Signed-off-by: umi_kernel_sm8250 <umi@aptusitu>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -389,6 +389,10 @@ static int max_extfrag_threshold = 1000;
 #ifdef CONFIG_BPF_SYSCALL
 static int bpf_stats_handler(struct ctl_table *table, int write,
 			     void __user *buffer, size_t *lenp,
+			     loff_t *ppos);
+
+static int bpf_stats_handler(struct ctl_table *table, int write,
+			     void __user *buffer, size_t *lenp,
 			     loff_t *ppos)
 {
 	struct static_key *key = (struct static_key *)table->data;
@@ -1725,6 +1729,7 @@ static struct ctl_table kern_table[] = {
 		.extra2		= &two,
 	},
 #endif
+#ifdef CONFIG_BPF_SYSCALL
 	{
 		.procname	= "bpf_stats_enabled",
 		.data		= &bpf_stats_enabled_key.key,
@@ -1732,6 +1737,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= bpf_stats_handler,
 	},
+#endif
 #if defined(CONFIG_TREE_RCU) || defined(CONFIG_PREEMPT_RCU)
 	{
 		.procname	= "panic_on_rcu_stall",
