e731e6d56e72fe125d4b658cffd7b5f7975a00de
Kernel Builder <builder@localhost>
2026-09-22 04:50:16 +0000

    drm: remove stub implementations from header
    The stub implementations were causing #endif without #if errors.
    They are not needed since we now properly guard all Xiaomi-specific
    code in drm_sysfs.c with CONFIG_DRM_MSM.
    
    Signed-off-by: umi_kernel_sm8250 <umi@aptusitu>

--- drivers/gpu/drm/drm_internal_mi.h
+++ drivers/gpu/drm/drm_internal_mi.h
@@ -61,25 +61,4 @@ ssize_t dsi_display_get_hw_vsync_info(struct drm_connector *connector,
 				char *buf);
 
 
-#else /* CONFIG_DRM_MSM */
-/* Stub implementations when CONFIG_DRM_MSM is not set */
-static inline int dsi_display_set_disp_param(struct drm_connector *c, u32 p) { return 0; }
-static inline int dsi_display_get_disp_param(struct drm_connector *c, u32 *p) { return 0; }
-static inline ssize_t dsi_display_write_mipi_reg(struct drm_connector *c, char *b) { return 0; }
-static inline ssize_t dsi_display_read_mipi_reg(struct drm_connector *c, char *b) { return 0; }
-static inline ssize_t dsi_display_read_oled_pmic_id(struct drm_connector *c, char *b) { return 0; }
-static inline ssize_t dsi_display_read_panel_info(struct drm_connector *c, char *b) { return 0; }
-static inline ssize_t dsi_display_read_wp_info(struct drm_connector *c, char *b) { return 0; }
-static inline ssize_t dsi_display_read_dynamic_fps(struct drm_connector *c, char *b) { return 0; }
-static inline int dsi_display_set_doze_brightness(struct drm_connector *c, int b) { return 0; }
-static inline int dsi_display_get_doze_brightness(struct drm_connector *c, char *b) { return 0; }
-static inline int dsi_display_read_gamma_param(struct drm_connector *c) { return 0; }
-static inline ssize_t dsi_display_print_gamma_param(struct drm_connector *c, char *b) { return 0; }
-static inline ssize_t dsi_display_fod_get(struct drm_connector *c, char *b) { return 0; }
-static inline ssize_t complete_commit_time_get(struct drm_connector *c, char *b) { return 0; }
-static inline int dsi_display_set_thermal_hbm_disabled(struct drm_connector *c, bool b) { return 0; }
-static inline int dsi_display_get_thermal_hbm_disabled(struct drm_connector *c, bool *b) { return 0; }
-static inline ssize_t dsi_display_get_hw_vsync_info(struct drm_connector *c, char *b) { return 0; }
-#endif
-
 #endif /*_DRM_INTERFACE_MI_H_*/
