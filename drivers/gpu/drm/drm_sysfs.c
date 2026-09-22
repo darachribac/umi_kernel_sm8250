263514e6fc5760d11c919ba2a02a8fc284d40bb4
Kernel Builder <builder@localhost>
2026-09-22 05:23:14 +0000

    drm: fix guard structure for device attributes
    Move basic DEVICE_ATTR (status, enabled, dpms, modes, disp_pcc) outside
    CONFIG_DRM_MSM guard. Only Xiaomi-specific attributes should be guarded.
    
    Signed-off-by: umi_kernel_sm8250 <umi@aptusitu>

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -538,6 +538,8 @@ static DEVICE_ATTR_RW(status);
 static DEVICE_ATTR_RO(enabled);
 static DEVICE_ATTR_RO(dpms);
 static DEVICE_ATTR_RO(modes);
+static DEVICE_ATTR_RW(disp_pcc);
+#endif /* CONFIG_DRM_MSM */
 static DEVICE_ATTR_RW(disp_param);
 static DEVICE_ATTR_RW(mipi_reg);
 static DEVICE_ATTR_RO(oled_pmic_id);
@@ -551,8 +553,6 @@ static DEVICE_ATTR_RO(smart_fps_value);
 static DEVICE_ATTR_RO(complete_commit_time);
 static DEVICE_ATTR_RW(thermal_hbm_disabled);
 static DEVICE_ATTR_RO(hw_vsync_info);
-#endif /* CONFIG_DRM_MSM */
-static DEVICE_ATTR_RW(disp_pcc);
 
 static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_status.attr,
